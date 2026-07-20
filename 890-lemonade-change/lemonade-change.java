8445186b4c1575ad47632b2b5dbaf12e2faf39c8
Tharshana <tharshanashanmugam17@gmail.com>
2026-07-20 23:19:05 +0530

    Time: 2 ms (99.01%) | Memory: 72.4 MB (48.51%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int c5=0,c10=0;
+        for (int i=0;i<bills.length;i++){
+            if (bills[i]==5)c5++;
+            else if (bills[i]==10){
+                if (c5>0){
+                    c5--;
+                    c10++;
+                }
+                else return false;
+            }
+            else {
+                if (c10>0 && c5>0){
+                    c10--;
+                    c5--;
+                }
+                else if (c5>=3) c5-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
